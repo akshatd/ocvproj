cf1cd0880e4daada1f3432023663e58e6fc0b042
Akshat <akshatdubey@outlook.com>
2017-05-04 16:56:28 +0800

    formatting

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
-using namespace
-cv;
-int
-main(int argc, char** argv ){
+
+using namespace cv;
+
+int main(int argc, char** argv ){
 	if( argc != 2 ){
 		printf("usage: DisplayImage.out <Image_Path>\n");
 		return -1;
