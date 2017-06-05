de3ee2003e7056b35d7163d6ec060e37313611a7
Akshat <akshatdubey@outlook.com>
2017-06-05 20:59:26 +0800

    brightness and contrast

--- test/testocv.cc
+++ test/testocv.cc
@@ -7,11 +7,15 @@ using namespace std;
 
 int main(int argc, char** argv ){
 	double start = (double)getTickCount();
-
+	double alpha = 1.0;
+	int beta = 0;
 	Mat img = imread(argv[1], IMREAD_COLOR);
-	Mat img2 = imread(argv[2], IMREAD_COLOR);
 	Mat out;
-	addWeighted(img, 0.5, img2, 0.5, 0.0, out);
+	cout << "Brightness: " ;
+	cin >> beta;
+	cout << "Contrast: " ;
+	cin >> alpha;
+	img.convertTo(out, -1, alpha, beta);
 	double elapsed = ((double)getTickCount()-start)/getTickFrequency();
 	cout << "Time taken " << elapsed << endl;
 	namedWindow("Org Image", WINDOW_NORMAL);
